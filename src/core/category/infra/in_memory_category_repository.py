b3889a03feee9930a05fb31156e415ddc0fd2173
Alex Oliveira <alexsoliveira@gmail.com>
2026-04-11 16:03:43 -0300

    feat: Refactor category use cases by moving CreateCategory and GetCategory logic to use_cases module; add GetCategory use case and related tests

--- src/core/category/infra/in_memory_category_repository.py
+++ src/core/category/infra/in_memory_category_repository.py
@@ -1,4 +1,6 @@
 from src.core.category.application.category_repository import CategoryRepository
+from src.core.category.domain.category import Category
+from uuid import UUID
 
 class InMemoryCategoryRepository(CategoryRepository):
     def __init__(self, categories=None):
@@ -6,3 +8,10 @@ class InMemoryCategoryRepository(CategoryRepository):
 
     def save(self, category):
         self.categories.append(category)
+
+    def get_by_id(self, id:UUID) -> Category | None:
+        return next((category for category in self.categories if category.id == id), None)
+        # for category in self.categories:
+        #     if category.id == id:
+        #         return category
+        # return None
---
+++ src/core/category/tests/application/integration/test_get_category.py
@@ -0,0 +1,63 @@
+import pytest
+import uuid
+from uuid import UUID
+from unittest.mock import MagicMock
+from src.core.category.application.use_cases.get_category import GetCategory, GetCategoryRequest, GetCategoryResponse
+from src.core.category.application.use_cases.exceptions import InvalidCategoryData, CategoryNotFound
+from src.core.category.infra.in_memory_category_repository import InMemoryCategoryRepository
+from src.core.category.domain.category import Category
+
+class TestGetCategory:
+    def test_get_category_by_id(self):
+        category_filme = Category(
+            name="Filme", 
+            description="Categoria para filmes",
+            is_active=True
+        )
+        category_serie = Category(
+            name="Série",
+            description="Categoria para séries",
+            is_active=True
+        )
+        repository = InMemoryCategoryRepository(
+            categories=[category_filme, category_serie]
+        )
+        use_case = GetCategory(repository=repository)
+        request = GetCategoryRequest(
+            id=category_filme.id
+        )
+
+        response = use_case.execute(request)
+
+        assert response == GetCategoryResponse(
+            id=category_filme.id,
+            name=category_filme.name,
+            description=category_filme.description,
+            is_active=category_filme.is_active,
+        )
+
+    def test_when_category_does_not_exist_should_raise_an_exception(self):
+        category_filme = Category(
+            name="Filme", 
+            description="Categoria para filmes",
+            is_active=True
+        )
+        category_serie = Category(
+            name="Série",
+            description="Categoria para séries",
+            is_active=True
+        )
+        repository = InMemoryCategoryRepository(
+            categories=[category_filme, category_serie]
+        )
+        use_case = GetCategory(repository=repository)
+        not_found_id = uuid.uuid4()
+        request = GetCategoryRequest(
+            id=not_found_id
+        )
+
+        with pytest.raises(CategoryNotFound) as exc_info:
+            use_case.execute(request)
+
+        
+        
