b773c968708882ff1b6674e7abce7bdf3fc20cc3
Alex Oliveira <alexsoliveira@gmail.com>
2026-04-11 18:54:20 -0300

    feat: Implement DeleteCategory use case and related tests; refactor category repository methods

---
+++ src/core/category/tests/application/use_cases/unit/test_get_category.py
@@ -0,0 +1,43 @@
+import pytest
+import uuid
+from uuid import UUID
+from unittest.mock import MagicMock
+from src.core.category.application.use_cases.get_category import GetCategory, GetCategoryRequest, GetCategoryResponse
+from src.core.category.application.use_cases.exceptions import InvalidCategoryData
+from src.core.category.application.category_repository import CategoryRepository
+from src.core.category.domain.category import Category
+from unittest.mock import create_autospec
+from src.core.category.application.use_cases.exceptions import CategoryNotFound
+
+class TestGetCategory:
+    def test_when_category_exists_then_return_response_dto(self):
+        mock_category = Category(
+            id=uuid.uuid4(),
+            name="Filme", 
+            description="Categoria para filmes",
+            is_active=True
+        )
+        mock_repository = create_autospec(CategoryRepository)
+        mock_repository.get_by_id.return_value = mock_category
+
+        use_case = GetCategory(repository=mock_repository)
+        request = GetCategoryRequest(id=mock_category.id)
+
+        response = use_case.execute(request)
+
+        assert response == GetCategoryResponse(
+            id=mock_category.id,
+            name=mock_category.name,
+            description=mock_category.description,
+            is_active=mock_category.is_active,
+        )
+        
+    def test_when_category_does_not_exist_then_raise_exception(self):
+        mock_repository = create_autospec(CategoryRepository)
+        mock_repository.get_by_id.return_value = None
+
+        use_case = GetCategory(repository=mock_repository)
+        request = GetCategoryRequest(id=uuid.uuid4())
+
+        with pytest.raises(CategoryNotFound):
+            use_case.execute(request)
