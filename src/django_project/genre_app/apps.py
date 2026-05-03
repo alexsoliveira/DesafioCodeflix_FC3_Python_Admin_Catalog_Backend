2f8f53e77f645a6ab9121ad1d2774b4e87780c91
Alex Oliveira <alexsoliveira@gmail.com>
2026-05-03 12:56:50 -0300

    feat: Add Genre model with migrations, admin registration, and initial tests

---
+++ src/django_project/genre_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GenreAppConfig(AppConfig):
+    name = 'src.django_project.genre_app'
--- src/django_project/genre_app/models.py
+++ src/django_project/genre_app/models.py
@@ -5,10 +5,13 @@ from uuid import uuid4
 class Genre(models.Model):
     app_label = 'genre_app'
 
-    id = models.UUIDField(primary_key=True, default=uuid4)
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     name = models.CharField(max_length=255)
-    categories = models.ManyToManyField('category_app.Category', related_name='genres')
     is_active = models.BooleanField(default=True)
+    categories = models.ManyToManyField('category_app.Category', related_name='genres')
 
     class Meta:
-        db_table = 'genres'
+        db_table = 'genre'
+
+    def __str__(self):
+        return self.name
