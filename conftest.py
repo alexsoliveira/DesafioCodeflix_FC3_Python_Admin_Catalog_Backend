a08caa23bccd21548a8e1273b8278ca43061a0f0
Alex Oliveira <alexsoliveira@gmail.com>
2026-05-16 16:39:02 -0300

    feat: Update test configuration and enhance genre deletion test with additional dependencies

--- conftest.py
+++ conftest.py
@@ -1,5 +1,6 @@
 import os
 import django
+import pytest
 from django.conf import settings
 from django.core.management import call_command
 
@@ -8,3 +9,11 @@ def pytest_configure():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.django_project.settings')
     django.setup()
     call_command("migrate", run_syncdb=True, verbosity=0)
+
+
+@pytest.fixture(autouse=True)
+def isolate_django_db(request):
+    if "django_db" in request.keywords:
+        call_command("flush", verbosity=0, interactive=False)
+
+    yield
